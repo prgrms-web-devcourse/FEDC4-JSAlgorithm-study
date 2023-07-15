e321bb0bfc0a0fef67943953d0336ec0596d24fd
Ahn Hyesu <linim@naver.com>
2023-07-15 14:39:14 +0900

    feat: [1차] 캐쉬 문제

---
+++ 1주차/[Lv.2] [1차] 캐쉬/안혜수.js
@@ -0,0 +1,19 @@
+function solution(cacheSize, cities) {
+    var answer = 0;
+    let cache = [];
+
+    if (cacheSize === 0) return cities.length * 5
+    cities.forEach(x => {
+        let city = x.toUpperCase();
+
+        if (cache.includes(city)) {
+            answer += 1;
+            cache.splice(cache.indexOf(city), 1);
+        } else {
+            answer += 5;
+            if (cache.length >= cacheSize) cache.shift();
+        }
+        cache.push(city);
+    })
+    return answer;
+}
