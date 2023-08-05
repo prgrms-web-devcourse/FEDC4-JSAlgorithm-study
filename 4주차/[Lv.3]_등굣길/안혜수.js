cae40d7b4d7d312a471416a7d379381b8eee806e
AhnHyesu <linim@naver.com>
2023-08-06 01:15:25 +0900

    등굣길 수정

--- 4주차/[Lv.3]_등굣길/안혜수.js
+++ 4주차/[Lv.3]_등굣길/안혜수.js
@@ -1,21 +1,19 @@
 function solution(m, n, puddles) {
-    var answer = 0;
-    let route = Array.from({ length: n }, () => Array(m).fill(0));
-    for (const pool of puddles) {
-        route[pool[1] - 1][pool[0] - 1] = 1;
+  var answer = 0;
+  let route = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
+  for (const pool of puddles) {
+    if (pool.length) route[pool[1]][pool[0]] = -1;
+  }
+  route[1][1] = 1;
+  for (let i = 1; i < route.length; i++) {
+    for (let j = 1; j < route[0].length; j++) {
+      if (i === 1 && j === 1) continue;
+      if (route[i][j] !== -1) {
+        if (route[i - 1][j] > 0) route[i][j] += route[i - 1][j];
+        if (route[i][j - 1] > 0) route[i][j] += route[i][j - 1];
+        route[i][j] %= 1000000007
+      }
     }
-    function dfs(y, x) {
-        if (y === n - 1 && x === m - 1) {
-            answer++;
-            answer %= 1000000007
-        }
-        if (x < m - 1) {
-            if (route[y][x + 1] !== 1) dfs(y, x + 1);
-        }
-        if (y < n - 1) {
-            if (route[y + 1][x] !== 1) dfs(y + 1, x);
-        }
-    }
-    dfs(0, 0);
-    return answer % 1000000007;
+  }
+  return route[n][m];
 }
