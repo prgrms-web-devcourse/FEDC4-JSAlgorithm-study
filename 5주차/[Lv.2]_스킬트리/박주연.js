4f7eb93018e5fb98888ea7c953d554162a4477e9
juyeon-park <lpjy1107@gmail.com>
2023-08-12 05:25:27 +0900

    feat : prettier 적용

--- 5주차/[Lv.2]_스킬트리/박주연.js
+++ 5주차/[Lv.2]_스킬트리/박주연.js
@@ -1,17 +1,17 @@
 function solution(skill, skill_trees) {
-    function checkSkill(value){
-        const order = skill.split(''); 
-        let idx = 0; 
-        
-        for (let i = 0; i < value.length ; i++){
-            // skill에 없는 종류가 있을때
-            if(!order.includes(value[i])) continue;
-            
-            // 순서 검사
-            if(value[i] !== order[idx++]) return false
-        }
-        return true;
+  function checkSkill(value) {
+    const order = skill.split('');
+    let idx = 0;
+
+    for (let i = 0; i < value.length; i++) {
+      // skill에 없는 종류가 있을때
+      if (!order.includes(value[i])) continue;
+
+      // 순서 검사
+      if (value[i] !== order[idx++]) return false;
     }
-    
-    return skill_trees.filter((v) => checkSkill(v)).length
+    return true;
+  }
+
+  return skill_trees.filter((v) => checkSkill(v)).length;
 }
--- 5주차/[Lv.3]_단속카메라/박주연.js
+++ 5주차/[Lv.3]_단속카메라/박주연.js
@@ -1,18 +1,18 @@
 function solution(routes) {
-    let ans = 0;
-    
-    // 진입 지점을 기준으로 2차원 배열 정렬
-    routes.sort((a,b) => a[0] - b[0])
-    
-    let camera = routes[0][1]; // -15
-    for(let i = 1; i < routes.length ; i++){
-        if(routes[i][0] <= camera){
-            if(routes[i][1] < camera) camera = routes[i][1]
-        }else{
-            ans++;
-            camera = routes[i][1]
-        }
+  let ans = 0;
+
+  // 진입 지점을 기준으로 2차원 배열 정렬
+  routes.sort((a, b) => a[0] - b[0]);
+
+  let camera = routes[0][1]; // -15
+  for (let i = 1; i < routes.length; i++) {
+    if (routes[i][0] <= camera) {
+      if (routes[i][1] < camera) camera = routes[i][1];
+    } else {
+      ans++;
+      camera = routes[i][1];
     }
-    // 마지막에 남는 카메라는 따로 +1 추가
-    return ans+1
+  }
+  // 마지막에 남는 카메라는 따로 +1 추가
+  return ans + 1;
 }
