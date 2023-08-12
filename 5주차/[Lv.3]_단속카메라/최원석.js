00437bf59b49136c2b62aa1d5f4b3b3c5705be1e
sukvvon <sukvvon@gmail.com>
2023-08-12 18:37:14 +0900

    refactor: update [Lv.3] 단속카메라's solution

--- 5주차/[Lv.3]_단속카메라/최원석.js
+++ 5주차/[Lv.3]_단속카메라/최원석.js
@@ -1,11 +1,11 @@
 function solution(routes) {
   routes.sort((a, b) => a[1] - b[1]);
 
-  let answer = 1;
-  let camera = routes[0][1];
+  let answer = 0;
+  let camera = -30001;
 
-  for (let i = 1; i < routes.length; i++) {
-    const [start, end] = routes[i];
+  for (const route of routes) {
+    const [start, end] = route;
 
     if (start <= camera) {
       continue;
