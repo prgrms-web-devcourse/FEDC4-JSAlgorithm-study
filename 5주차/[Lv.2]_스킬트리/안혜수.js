372cf15e22c3f84eb18d8b3e230522a91be78d50
sukvvon <sukvvon@gmail.com>
2023-08-22 02:34:55 +0900

    Add prettier in 5주차/**/안혜수.js

--- 5주차/[Lv.2]_스킬트리/안혜수.js
+++ 5주차/[Lv.2]_스킬트리/안혜수.js
@@ -1,10 +1,13 @@
 function solution(skill, skill_trees) {
-  const reg = new RegExp(`[${skill}]`,'g');
-  return skill_trees.map(x => {
-      if(x.match(reg)){
-          const result = x.match(reg).join("");
-          if(skill.slice(0,result.length)===result)return true;
-          return false;
-      }return true;
-  }).filter(x => x).length;
+  const reg = new RegExp(`[${skill}]`, 'g');
+  return skill_trees
+    .map((x) => {
+      if (x.match(reg)) {
+        const result = x.match(reg).join('');
+        if (skill.slice(0, result.length) === result) return true;
+        return false;
+      }
+      return true;
+    })
+    .filter((x) => x).length;
 }
--- 5주차/[Lv.3]_단속카메라/안혜수.js
+++ 5주차/[Lv.3]_단속카메라/안혜수.js
@@ -1,13 +1,13 @@
 function solution(routes) {
   const result = [];
-  routes.sort((a,b) => b[0]-a[0]);
-  for(let i = routes.length-1; i>0; i--){
-      if(routes[i][1]>=routes[i-1][0]){
-          const last_way = routes.pop();
-          routes[i-1][1] = last_way[1]>routes[i-1][1]?routes[i-1][1]:last_way[1];
-      }else{
-          result.push(routes.pop());
-      }
+  routes.sort((a, b) => b[0] - a[0]);
+  for (let i = routes.length - 1; i > 0; i--) {
+    if (routes[i][1] >= routes[i - 1][0]) {
+      const last_way = routes.pop();
+      routes[i - 1][1] = last_way[1] > routes[i - 1][1] ? routes[i - 1][1] : last_way[1];
+    } else {
+      result.push(routes.pop());
+    }
   }
-  return routes.length+result.length;
+  return routes.length + result.length;
 }
