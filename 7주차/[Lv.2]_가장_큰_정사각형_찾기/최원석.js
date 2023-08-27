3891ca83f78727a58c71acac3ecc348af6e0d371
sukvvon <sukvvon@gmail.com>
2023-08-27 19:09:55 +0900

    refactor: update [Lv.2] 가장 큰 정사각형 찾기's solution

--- 7주차/[Lv.2]_가장_큰_정사각형_찾기/최원석.js
+++ 7주차/[Lv.2]_가장_큰_정사각형_찾기/최원석.js
@@ -1,10 +1,8 @@
 function solution(board) {
-  const rowLen = board.length;
-  const colLen = board[0].length;
   let answer = 0;
 
-  for (let i = 0; i < rowLen; i++) {
-    for (let j = 0; j < colLen; j++) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
       if (i > 0 && j > 0 && board[i][j]) {
         board[i][j] = Math.min(board[i - 1][j], board[i][j - 1], board[i - 1][j - 1]) + 1;
       }
